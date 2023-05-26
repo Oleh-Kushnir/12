a9e0db793b25183681b28239087ff1cc6cc59b48
Oleh-Kushnir <110404760+Oleh-Kushnir@users.noreply.github.com>
2023-05-26 22:01:32 +0300

    123

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ProfileStyled = styled.div`
-  padding: 10px;
+  padding: 5px;
   width: 300px;
   background-color: #fff;
   border: #e4e4e4 solid 1px;
